6635468db8e01ce18e86a57468549f1cf8c6cd98
王著奎台式机 <taishiji@qq.com>
2025-11-04 00:17:12 +0800

    第五次提交

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # 添加这个视图函数
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# from django.shortcuts import render
+# from django.utils import timezone
+# from .models import Post
+
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#     return render(request, 'blog/post_list.html', {})
